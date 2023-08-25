0fc5467a414744d3ad79cf537042cd9dbaded38b
hd54 <viethung5403@gmail.com>
2023-08-25 15:49:50 -0700

    Partial CSS work
    TODO: front-end

--- backrooms_site/frontend/src/components/App.js
+++ backrooms_site/frontend/src/components/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Test</h1>;
+        return <HomePage />;
     }
 }
 
---
+++ backrooms_site/frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // template
+    render() {
+        return <h1>Hi</h1>
+    }
+}
